27896ff1e1ae3944d153bf884af40d481ece8717
Marlon Lückert <m.lueckert@me.com>
2018-05-21 01:56:59 +0200

    browser testing with travis, use Firefox instead of Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -46,7 +46,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
